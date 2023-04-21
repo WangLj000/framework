1af70a823acf78a60d58477dedbc376b42071f5d
wanglongjiang <wanglongjiang@skyworth.com>
2023-04-21 13:49:44 +0800

    Initial commit

---
+++ app/src/main/java/com/android/test/MainActivity.java
@@ -0,0 +1,33 @@
+package com.android.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.IPackageDataObserver;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.RemoteException;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clear(){
+        PackageManager pm = getPackageManager();
+        List<PackageInfo> packlist = pm.getInstalledPackages(PackageManager.GET_ACTIVITIES);
+        for (PackageInfo packageName:packlist) {
+            pm.deleteApplicationCacheFiles(packageName.packageName, new IPackageDataObserver.Stub() {
+                @Override
+                public void onRemoveCompleted(final String packageName, final boolean succeeded) throws
+                        RemoteException {
+                }
+            });
+        }
+    }
+}
